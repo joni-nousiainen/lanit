469a80edf3d8beda74a352fcf7ed26814a667d3c
Joni Nousiainen <developer@joninousiainen.info>
2017-09-11 20:47:19 +0300

    Fixed Windows Phone 8 IE 10 viewport problem.

--- src/main/resources/static/code.js
+++ src/main/resources/static/code.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
+  // Copyright 2014-2017 The Bootstrap Authors
+  // Copyright 2014-2017 Twitter, Inc.
+  // Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.head.appendChild(msViewportStyle)
+  }
+
   // For busting AJAX cache on Windows Phone browser
   $.ajaxSetup({ cache: false })
 
